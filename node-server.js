b5dc5c4e7c00855d31ddd92ec5ea4725c840e763
radharay <radharayg@gmail.com>
2019-07-20 17:31:31 -0300

    Instalando package.json e express

--- node-server.js
+++ node-server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-res.end('Sou um servidor criado pelo Node.js!\n');
+res.end('Hello! Sou um servidor criado pelo Node.js!\n');
 }).listen(3000, '127.0.0.1');
 
 /* Agora, rode o script no terminal através do comando node node-server.js(rode o seu arquivo)
